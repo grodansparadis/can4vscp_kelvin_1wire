dafdb854a42c4877c8f656bdcc1e058783c289d0
Ake Hedman <akhe@grodansparadis.com>
2016-03-08 23:26:14 +0100

    DS1820 code

--- onewire.h
+++ onewire.h
@@ -413,6 +413,60 @@
 #define SELECT_ADC_TEMP4                    (9<<2)   // ADC9
 #define SELECT_ADC_TEMP5                    (8<<2)   // ADC8
 
+
+// ----------------------------------------------------------------------------- 
+//                                DS1820                                      
+// ----------------------------------------------------------------------------- 
+
+
+#define TEMP_RES              0x100 // temperature resolution => 1/256°C = 0.0039°C 
+
+
+// -----------------------------------------------------------------------------
+//                         DS1820 Timing Parameters                           
+// -----------------------------------------------------------------------------
+
+#define DS1820_RST_PULSE       480   // master reset pulse time in [us] 
+#define DS1820_MSTR_BITSTART   2     // delay time for bit start by master 
+#define DS1820_PRESENCE_WAIT   40    // delay after master reset pulse in [us] 
+#define DS1820_PRESENCE_FIN    480   // dealy after reading of presence pulse [us] 
+#define DS1820_BITREAD_DLY     5     // bit read delay 
+#define DS1820_BITWRITE_DLY    100   // bit write delay 
+
+
+// -----------------------------------------------------------------------------
+//                            DS1820 Registers                                
+// -----------------------------------------------------------------------------
+
+#define DS1820_REG_TEMPLSB    0
+#define DS1820_REG_TEMPMSB    1
+#define DS1820_REG_CNTREMAIN  6
+#define DS1820_REG_CNTPERSEC  7
+#define DS1820_SCRPADMEM_LEN  9     // length of scratchpad memory 
+
+#define DS1820_ADDR_LEN       8
+
+
+// -----------------------------------------------------------------------------
+//                            DS1820 Commands                                 
+// -----------------------------------------------------------------------------
+
+#define DS1820_CMD_SEARCHROM     0xF0
+#define DS1820_CMD_READROM       0x33
+#define DS1820_CMD_MATCHROM      0x55
+#define DS1820_CMD_SKIPROM       0xCC
+#define DS1820_CMD_ALARMSEARCH   0xEC
+#define DS1820_CMD_CONVERTTEMP   0x44
+#define DS1820_CMD_WRITESCRPAD   0x4E
+#define DS1820_CMD_READSCRPAD    0xBE
+#define DS1820_CMD_COPYSCRPAD    0x48
+#define DS1820_CMD_RECALLEE      0xB8
+
+
+#define DS1820_FAMILY_CODE_DS18B20      0x28
+#define DS1820_FAMILY_CODE_DS18S20      0x10
+
+
 // Temeparture conversions
 double Celsius2Fahrenheit(double tc);
 double Fahrenheit2Celsius(double tf);
